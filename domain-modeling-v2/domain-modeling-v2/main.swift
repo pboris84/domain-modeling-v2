8cd4df053d327f98b34d37528e418f64fdc08721
Boris Pavlov <pboris84@gmail.com>
2016-04-19 17:08:00 -0700

    Final

---
+++ domain-modeling-v2/domain-modeling-v2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling-v2
+//
+//  Created by iGuest on 4/19/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
